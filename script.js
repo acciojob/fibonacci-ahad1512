be603cafa641cb3fe9410b5c95fd205ceb5075d1
ahad1512 <139266271+ahad1512@users.noreply.github.com>
2024-04-07 03:02:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function fibonacci(num) {
 	let first =0;
 	let second =1;
 	let term =0;
+	if (num===1) {
+		return 0;
+	}
+	if (num===2) {
+		return 1;
+	}
 	for (let i = 1; i < num-1; i++) {
 		term = first+second;
 		first=second;
